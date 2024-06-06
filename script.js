cf3ac18efbc72322fc898157580d9d682638769d
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 19:48:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function increment() {
 	let value = document.getElementById("counter").innerHTML
-	document.getElementById("counter").innerHTML = Number(value)+1
 	alert(value)
+	document.getElementById("counter").innerHTML = Number(value)+1
 }

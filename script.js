2809b57bc23304da67f15fde9f24623ffa220243
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 19:42:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function increment() {
+	let value = document.getElementById("counter").innerHTML
+	document.getElementById("counter").innerHTML = Number(value)+1
+	alert(value)
+}
